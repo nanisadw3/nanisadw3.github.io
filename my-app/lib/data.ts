9b81d7ad3d9ec53387d74506b9f8dacf02567c64
Inaki Sobera <nanisadw3@github.io>
2026-03-17 14:35:34 -0600

    feat: incluir imagen de pokemon y actualizar ruta

--- my-app/lib/data.ts
+++ my-app/lib/data.ts
@@ -38,7 +38,7 @@ export const portfolioData = {
     {
       title: "Pokémon Adivina Quién (Multijugador)",
       description: "Juego multijugador en tiempo real que utiliza la PokeAPI para un desafío de adivinar el Pokémon. Desarrollé tanto el frontend interactivo como el servidor para gestionar las partidas en red.",
-      image: "pokemon.png",
+      image: "images/pokemon.png",
       github: "https://github.com/nanisadw3/Pokemon_game",
       demo: "https://oyster-app-xwu29.ondigitalocean.app/",
     },
